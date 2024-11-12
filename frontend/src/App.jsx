560d5a6cea2b12ce1a5887a6f233dfff039cdd9e
Taral Shah <taral@gmail.com>
2024-11-13 00:49:16 +0530

    adding overview in dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import EmployerPage from "./pages/EmployerPage/EmployerPage"
 import LoginPage from './pages/LoginPage/LoginPage'
 import SignupPage from "./pages/SignupPage/SignupPage"
 import FindJob from './pages/FindJob/FindJob'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
   const location = useLocation();
@@ -19,12 +20,13 @@ function App() {
     <>
       {!hideNavbarFooter && <Navbar />}
       <Routes>
-        <Route path="/find-job" element={<FindJob/>}></Route>
+        <Route path="/find-job" element={<FindJob />}></Route>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/jobs" element={<JobPage />}></Route>
         <Route path="/employers" element={<EmployerPage />}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
       </Routes>
       {!hideNavbarFooter && <Footer />}
     </>
---
+++ frontend/src/components/SettingsComp/SettingsComp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./SettingsComp.css"
+
+const SettingsComp = () => {
+  return (
+    <div>
+      SettingsComp
+    </div>
+  )
+}
+
+export default SettingsComp
